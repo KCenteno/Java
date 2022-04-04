2c5bf0f4d8de329def7604c431d369ac09d65df1
Kevin Centeno <KevinCenteno_90@yahoo.com>
2022-04-04 15:21:50 -0400

    First java commit

---
+++ foundation/firstJavaProgram/Me.java
@@ -0,0 +1,10 @@
+public class Me {
+    public static void main(String[] args) {
+
+        System.out.println("My name is Kevin Centeno");
+
+        System.out.println("I am 32 years old");
+
+        System.out.println("My hometown is Bayamon, PR");
+    }
+}
