8c85074472e976b2e0b357774e367c05f25e9c7d
Kevin Centeno <KevinCenteno_90@yahoo.com>
2022-04-04 20:29:57 -0400

    alfredbot commit

---
+++ foundation/alfredBot/AlfredQuotes.java
@@ -0,0 +1,30 @@
+import java.util.Date;
+public class AlfredQuotes {
+    
+    public String basicGreeting() {
+        // You do not need to code here, this is an example method
+        return "Hello, lovely to see you. How are you?";
+    }
+    
+    public String guestGreeting(String name) {
+        // YOUR CODE HERE
+        return "Hello, " + name;
+    }
+    
+    public String dateAnnouncement() {
+        // YOUR CODE HERE
+        Date date = new Date();
+        return "The Date is, " + date;
+    }
+    
+    public String respondBeforeAlexis(String conversation) {
+        // YOUR CODE HERE
+        return conversation;
+    }
+    
+	// NINJA BONUS
+	// See the specs to overload the guessGreeting method
+    // SENSEI BONUS
+    // Write your own AlfredQuote method using any of the String methods you have learned!
+}
+
