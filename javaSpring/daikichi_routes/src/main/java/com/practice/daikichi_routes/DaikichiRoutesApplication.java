f647af269c633bcc06a61acc08f7bc4507d3a87c
Kevin Centeno <KevinCenteno_90@yahoo.com>
2022-04-09 21:26:51 -0400

    routes

--- javaSpring/daikichi_routes/src/main/java/com/practice/daikichi_routes/DaikichiRoutesApplication.java
+++ javaSpring/daikichi_routes/src/main/java/com/practice/daikichi_routes/DaikichiRoutesApplication.java
@@ -2,7 +2,9 @@ package com.practice.daikichi_routes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -32,6 +34,19 @@ public class DaikichiRoutesApplication {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	}
 	
+	@RequestMapping("/daikichi/travel/{destination}")
 	
-
+	public String travel(@PathVariable("destination") String destination) {
+		return "Congratulations! You will soon travel to " + destination + "!";
+	}
+	
+	@RequestMapping("/daikichi/lotto/{num}")
+	
+	public String lotto(@PathVariable("num") int num) {
+		if(num%2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		}
+		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends";
+	}
+	
 }
