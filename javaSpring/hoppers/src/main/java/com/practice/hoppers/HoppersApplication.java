69172935ddc767f5dfab6bcc1c1aa7d328f887c6
Kevin Centeno <KevinCenteno_90@yahoo.com>
2022-04-10 01:30:30 -0400

    hopper

---
+++ javaSpring/hoppers/src/main/java/com/practice/hoppers/HoppersApplication.java
@@ -0,0 +1,13 @@
+package com.practice.hoppers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoppersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoppersApplication.class, args);
+	}
+
+}
---
+++ javaSpring/hoppers/src/test/java/com/practice/hoppers/HoppersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.hoppers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
