7f1ece499d27265e81a25ad777b323a704c79824
Kevin Centeno <KevinCenteno_90@yahoo.com>
2022-04-18 10:10:18 -0400

    took max out commit

--- javaSpring/Dojos/src/main/java/com/practice/dojo/models/Ninja.java
+++ javaSpring/Dojos/src/main/java/com/practice/dojo/models/Ninja.java
@@ -13,7 +13,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
