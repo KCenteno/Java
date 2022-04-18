5b0fdea86a406d62e41e0f6a7da9865a3f037054
Kevin Centeno <KevinCenteno_90@yahoo.com>
2022-04-18 10:12:52 -0400

    travel change

--- javaSpring/SafeTravels/src/main/java/com/practice/safetravels/controllers/TravelPlanController.java
+++ javaSpring/SafeTravels/src/main/java/com/practice/safetravels/controllers/TravelPlanController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RequestParam;
 
 import com.practice.safetravels.models.TravelPlan;
 import com.practice.safetravels.services.TravelPlanService;
